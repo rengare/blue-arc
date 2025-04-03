d2fcb0afa005933a89086dadc5668b66d910cda3
ggorecki <gorecki.gracjan@gmail.com>
2025-04-03 15:17:54 +0200

    keyd

--- build_files/build.sh
+++ build_files/build.sh
@@ -15,6 +15,11 @@ dnf5 -y copr enable ryanabx/cosmic-epoch
 dnf5 -y install cosmic-desktop cosmic-store cosmic-edit cosmic-player
 dnf5 -y copr disable ryanabx/cosmic-epoch
 
+dnf5 -y copr enable refi64/keyd-git
+dnf5 -y install keyd
+systemctl enable keyd
+dnf5 -y copr disable refi64/keyd-git
+
 dnf5 -y install ptyxis kitty gnome-keyring-pam NetworkManager-tui \
   NetworkManager-openvpn xdg-user-dirs \
   emacs
